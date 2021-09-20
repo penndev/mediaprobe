623aaa63680ca9794b51842863beb5fa38820357
penn <pennilessfor@gmail.com>
2021-09-20 14:50:49 +0800

    add tree

--- main.py
+++ main.py
@@ -5,18 +5,42 @@ from PySide6 import QtCore, QtWidgets, QtGui
 class MainWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
-
-        self.button = QtWidgets.QPushButton("Select File")
-
         self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.button)
+
+        # 打开文件按钮
+        self.button = QtWidgets.QPushButton("Select File")
         self.button.clicked.connect(self.openFile)
+        self.layout.addWidget(self.button)
+
+        # 展示flv文件详情
+        self.tree = QtWidgets.QTreeWidget()
+        self.tree.setColumnCount(2)
+        self.tree.setHeaderLabels(["flv Tag", "File Body"])
+        # self.tree.insertTopLevelItems(0, self.getFileItem())
+        self.layout.addWidget(self.tree)
+
 
     @QtCore.Slot()
     def openFile(self):
         directory = QtWidgets.QFileDialog.getOpenFileName(self, "Find Files",
                 QtCore.QDir.currentPath(),'*.flv')
-        print(directory)
+        if directory:
+            self.tree.insertTopLevelItems(0, self.getFileItem())
+    
+    @QtCore.Slot()
+    def getFileItem(self):
+        data = {"Project A": ["file_a.py", "file_a.txt", "something.xls"],
+                "Project B": ["file_b.csv", "photo.jpg"],
+                "Project C": []}
+        items = []
+        for key, values in data.items():
+            item = QtWidgets.QTreeWidgetItem([key])
+            for value in values:
+                ext = value.split(".")[-1].upper()
+                child = QtWidgets.QTreeWidgetItem([value, ext])
+                item.addChild(child)
+            items.append(item)
+        return items
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 
 > 使用python + PySide6 开发的适用三端windows mac linux 的 flv媒体文件分析 桌面分析工具
 
-python3 -m venv env
-source env/bin/activate
+`python3 -m venv env`
+`source env/bin/activate`
 
 pip install -i https://mirrors.aliyun.com/pypi/simple/
