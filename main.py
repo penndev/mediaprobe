d79bb8125a184dc9676a453929d17fdc79e93d7e
penn <pennilessfor@gmail.com>
2021-09-22 20:24:07 +0800

    可筛选文件

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-import random
+from os import read
 from PySide6 import QtCore, QtWidgets, QtGui
 
 class MainWidget(QtWidgets.QWidget):
@@ -12,46 +11,47 @@ class MainWidget(QtWidgets.QWidget):
         self.button.clicked.connect(self.openFile)
         self.layout.addWidget(self.button)
 
-        # 展示flv文件详情
+        # 展示flv文件详情的面板
         self.tree = QtWidgets.QTreeWidget()
         self.tree.setColumnCount(2)
         self.tree.setHeaderLabels(["flv Tag", "File Body"])
-        # self.tree.insertTopLevelItems(0, self.getFileItem())
         self.layout.addWidget(self.tree)
 
 
     @QtCore.Slot()
     def openFile(self):
-        directory = QtWidgets.QFileDialog.getOpenFileName(self, "Find Files",
-                QtCore.QDir.currentPath(),'*.flv')
+        directory = QtWidgets.QFileDialog.getOpenFileName(self, "Find Files", " ",'*.flv')
         if directory:
             self.tree.insertTopLevelItems(0, self.getFileItem(directory))
     
     @QtCore.Slot()
     def getFileItem(self,flvFilePath):
-        # print(flvFilePath)
         items = []
-        with open(flvFilePath[0], 'rt') as f:
-            data = f.read(1).hex()
-            
-            print(data)
-            item = QtWidgets.QTreeWidgetItem(["Flv Header"])
-            item.addChild(QtWidgets.QTreeWidgetItem(["hex", data]))
+        with open(flvFilePath[0], 'rb') as f:
+            data = f.read(9)
+            data = data.hex(" ")
+            item = QtWidgets.QTreeWidgetItem(["Flv Header",data])
+            item.addChild(QtWidgets.QTreeWidgetItem(["PreviousTagSize", f.read(4).hex(" ") ]))
             items.append(item)
+            while(True):
+                data = f.read(11)
+                if(data == b''):
+                    break
+                if(data[0] == 18):
+                    tagName = "Script Tag"
+                elif(data[0] == 9):
+                    tagName = "Video Tag"
+                elif(data[0] == 8):
+                    tagName = "Audit Tag"
+                else:
+                    break
+                item = QtWidgets.QTreeWidgetItem([tagName, data.hex(" ") ])
+                len = int.from_bytes(data[1:4], byteorder='big')
+                item.addChild(QtWidgets.QTreeWidgetItem(["body",f.read(len).hex(" ")]))
+                item.addChild(QtWidgets.QTreeWidgetItem(["PreviousTagSize", f.read(4).hex(" ") ]))
+                items.append(item)
         return items
 
-        # data = {"Project A": ["file_a.py", "file_a.txt", "something.xls"],
-        #         "Project B": ["file_b.csv", "photo.jpg"],
-        #         "Project C": []}
-        
-        # for key, values in data.items():
-        #     item = QtWidgets.QTreeWidgetItem([key])
-        #     for value in values:
-        #         ext = value.split(".")[-1].upper()
-        #         child = QtWidgets.QTreeWidgetItem([value, ext])
-        #         item.addChild(child)
-        #     items.append(item)
-        # return items
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
@@ -60,4 +60,4 @@ if __name__ == "__main__":
     widget.resize(800, 600)
     widget.show()
 
-    sys.exit(app.exec())
+    app.exec()
