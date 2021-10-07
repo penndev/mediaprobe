5a6dc6e9c9a1e55117cc2a6c3ae6fb5b100b5e55
penn <pennilessfor@gmail.com>
2021-10-07 16:23:52 +0800

    解析nalu数据

--- flvstruct.py
+++ flvstruct.py
@@ -1,6 +1,30 @@
 # 官方flv标准介绍
 # https://www.adobe.com/content/dam/acom/en/devnet/flv/video_file_format_spec_v10.pdf
 
+class AVCPacket:
+    Type = None
+    CompositionTime = None
+    Data = None
+    def __init__(self, data):
+        self.Type = data[0]
+        self.CompositionTime = int.from_bytes(data[1:4], byteorder='big')
+        if self.Type == 0:
+            print("遇到了avcdecoder")
+            #AVCDecoderConfigurationRecord
+        elif self.Type == 1:
+            n = 4
+            while True:
+                if n == len(data):
+                    break
+                naluLen = int.from_bytes(data[n:n+4], byteorder='big')
+                endSize = n+4+naluLen
+                nalu = data[n+4:endSize]
+                n = endSize
+                print(n)
+        else:
+            pass
+        
+
 class FlvHeader:
     data = None
     tagSize = None
@@ -25,20 +49,26 @@ class FlvTag:
         self.streamID = data[8:11]
     def setData(self,data):
         self.data = data
-        self.export()
-    def setPreviousTagSize(self,data):
-        self.tagSize = int.from_bytes(data, byteorder='big')
-    # 拆分出aac和avc数据
-    def export(self):
+        # 拆分出aac和avc数据
         if(self.tagType == 18):
-            self.exportScript() 
+            self.onMetaData() 
         elif self.tagType == 9:
-            self.exportVideo()
+            self.videoTag()
         elif self.tagType == 8:
-            self.exportAudit()        
+            self.audioTag()      
         else:
             pass
-    
+    def setPreviousTagSize(self,data):
+        self.tagSize = int.from_bytes(data, byteorder='big')
+    def onMetaData(self):
+        pass
+    def audioTag(self):
+        pass
+    def videoTag(self):
+        self.FrameType = self.data[0] >> 4
+        self.CodecID = self.data[0] & 0xf
+        if(self.CodecID == 7):#h264
+            AVCPacket(self.data[1:])
 
 class Flv:
     header = None
@@ -57,3 +87,5 @@ class Flv:
                 tag.setData(f.read(tag.dataSize))
                 tag.setPreviousTagSize(f.read(4))
                 self.body.append(tag)
+
+Flv("test.flv")
