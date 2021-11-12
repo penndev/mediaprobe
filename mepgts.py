d7371a7e67572cfd0beca502259abd332c05ac01
Penn <pennilessfor@gmail.com>
2021-11-12 10:33:55 +0800

    add mpegts decode

---
+++ mepgts.py
@@ -0,0 +1,53 @@
+
+class TsPacketHeader:
+    def __init__(self,data) -> None:
+        self.sync = data[0]
+        self.error = data[1] >> 7  #是否报错
+        self.payloadStart = data[1] >> 6 & 1 
+        self.priority = data[1] >> 5 & 1 #优先级
+        data[1] &= 0x1f
+        self.pid = int.from_bytes(data[1:3],"big")
+        self.scrambling = data[3] >> 6 #传输加扰。
+        self.adaption = data[3] >> 4 & 3
+        self.continuity = data[3] & 0x0f
+
+class TsPacket:
+    def __init__(self,data) -> None:
+        'adaption 01{1}仅含有效负载，10{2}仅含调整字段，11{3}含有调整字段和有效负载。为00解码器不进行处理'
+        self.header = TsPacket(data[0:4])
+        self.body = []
+        if self.header.adaption >= 2:#存在调整字段
+            self.adaptLen = data[4]
+            self.adapt = data[5:5+self.adaptLen]
+            if self.header.adaption == 3:#同时存在es
+                self.body = data[5+self.adaptLen:]
+            else:
+                self.body = []
+        elif self.header.adaptLen == 1:
+            if self.header.payloadStart == 1:
+                startLen = 5 + data[5]
+                self.body = data[startLen:]
+            else:
+                self.body = data[4:]
+
+class Ts:
+    def __init__(self,pwd) -> None:
+        self.body = []
+        with open(pwd,'rb') as tsf:
+            while True:
+                tspck = tsf.read(188)
+                if tspck == b'':
+                    break
+                self.body.append(TsPacket(tspck))
+
+# class PES:
+#     def __init__(self) -> None:
+#         pass
+
+ts = Ts("test.ts")
+
+# for pkg in ts.body:
+    # 只取视频的pes
+
+
+# 读取所有视频的pes数据。
