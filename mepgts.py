aa5efb1b9023b51cd166956a4e8eaac2b98b91cf
Penn <pennilessfor@gmail.com>
2021-11-17 18:38:14 +0800

    gen pes

--- mepgts.py
+++ mepgts.py
@@ -30,6 +30,9 @@ class TsPacket:
             else:
                 self.body = data[4:]
 
+# ts = Ts("test.ts")
+# for pkg in ts.body:
+#     pass
 class Ts:
     def __init__(self,pwd) -> None:
         self.body = []
@@ -40,8 +43,64 @@ class Ts:
                     break
                 self.body.append(TsPacket(tspck))
 
+class TS:
+    Sync = 0x47
 
-ts = Ts("test.ts")
+def PAT():
+    bt = bytearray([0xff]*188)
+    hex = [
+        0x47, 0x40, 0x00, 0x10, #ts packet hd
+        0x00, #adaption
+        0x00, 0xB0, 0x0D, 0x00, 0x01, 0xC1, 0x00, 0x00, 0x00, 0x01, 
+        0xF0, 0x00, 0x2A, 0xB1, 0x04, 0xB2
+    ]
+    bt[0:20] = hex
+    return bt
 
-for pkg in ts.body:
-    pass
+def PMT():
+    bt = bytearray([0xff]*188)
+    hex = [
+        0x47, 0x50, 0x00, 0x10,
+        0x00,
+        0x02, 0xB0, 0x17, 0x00, 0x01, 0xC1, 0x00, 0x00, 0xE1, 0x00,
+        0xF0, 0x00, 0x1B, 0xE1, 0x00, 0xF0, 0x00, 0x0F, 0xE1, 0x01,
+        0xF0, 0x00, 0x2F, 0x44, 0xB9, 0x9B
+    ]
+    bt[0:30] = hex
+    return bt
+
+class PES():
+
+    _StartCode = [0x00, 0x00, 0x01]
+    _StreamId = {"video":0xe0,"audit":0xc0}
+    _PESLength = [0x00,0x00]
+    def __init__(self,pts,dts) -> None:
+        self.data = bytearray([0]*188)
+        self.data[0:3] = [0x00, 0x00, 0x01]
+        self.data[3] = 0xe0
+        self.data[4:6] = [0x00,0x00]
+        self.data[6] = 0x80 # pes-info信息
+        self.data[7] = 0xc0 # 默认存在dts和pts
+        self.data[8] = 0x0a # len
+
+        # pts
+        self.data[9] = (3 << 4) | ((pts>>30) << 1) | 1
+        gc = ((pts >> 15) & 0x7fff) << 1 | 1
+        self.data[10] = gc >> 8
+        self.data[11] = gc & 0xff
+        gc = ((pts & 0x7fff) << 1 ) | 1
+        self.data[12] = gc >> 8
+        self.data[13] = gc & 0xff
+
+        # dts
+        self.data[14] = (1 << 4) | ((dts>>30) << 1) | 1
+        gc = ((dts >> 15) & 0x7fff) << 1 | 1
+        self.data[15] = gc >> 8
+        self.data[16] = gc & 0xff
+        gc = ((dts & 0x7fff) << 1 ) | 1
+        self.data[17] = gc >> 8
+        self.data[18] = gc & 0xff
+
+a = PES(131940,126000)
+
+print(a.data.hex(" "))
