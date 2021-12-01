428d84a4513912a6fde56cd0f3d89d720db137a3
Penn <pennilessfor@gmail.com>
2021-12-01 17:13:25 +0800

    完成了flv to ts 容器转换

--- flv.py
+++ flv.py
@@ -1,6 +1,6 @@
-'官方flv标准介绍  https://www.adobe.com/content/dam/acom/en/devnet/flv/video_file_format_spec_v10.pdf'
-
 '''
+官方flv标准介绍  https://www.adobe.com/content/dam/acom/en/devnet/flv/video_file_format_spec_v10.pdf
+
 提取adts
 adtsHeader = [0xff, 0xf1, 0x4c, 0x80,0x00,0x00,0xfc]
 adtsLen = len(tag.tag.Data)+7
@@ -9,133 +9,133 @@ adtsLen = adtsLen | 0x1f
 adtsHeader[4:6] = adtsLen.to_bytes(2,'big')
 h.write(bytes(adtsHeader))
 h.write(bytes(tag.tag.Data))
-'''
 
-'''
 提取nalu
 0,0,0,1 + nalu.data
+
 '''
 
-naluHead = bytearray([0,0,0,1])
+from os import close
 
-naluSeq = bytearray()
-naluSeqTmp = bytearray()
 
-class FlvTag:
-    tagTypeItem = {8:"Audit Tag",9:"Video Tag",18:"Script Tag"}
-    def __init__(self,data) -> None:
-        '分析flv tag header 内容获取datasize'
-        self.data = data
+class TAG:
+    
+    NALU_HEAD = bytearray([0,0,0,1])
+
+    def getHead(self,data):
         self.tagType = data[0]
         self.dataSize = int.from_bytes(data[1:4], byteorder='big')
         self.timeStamp = int.from_bytes(data[4:7], byteorder='big')
         self.timeStampExtended = data[7]
         self.streamID = int.from_bytes(data[8:11], byteorder='big')
-        self.frameType = None
-        self.codecID = None
-        self.avcPacketType = None
-        self.nalu = []
-
+    
     def setData(self,data):
-        global naluSeq
-        global naluSeqTmp
-        naluSeqTmp = bytearray()
-        '解码媒体源数据'
-        self.data += data
-        if(self.tagType == 9):
+        if(self.tagType == 9): # flv video tag data
             self.frameType = data[0] >> 4
-            # print(data[0],self.frameType)
             self.codecID = data[0] & 0x0f
-            if(self.codecID == 7):
+            if(self.codecID == 7): # video type avc
+                self.nalu = bytearray()
                 self.avcPacketType = data[1]
                 self.compositionTime = int.from_bytes(data[2:5], byteorder='big')
-                i = 5
-                if (self.avcPacketType == 1):
-                    # self.nalu = data[i:]
-                    while(True):
-                        naluLen = int.from_bytes(data[i:i+4], byteorder='big')
-                        i = i + 4
-                        if(len(data) <= i):
-                            break
-                        self.nalu += list(naluHead) + list(data[i:i+naluLen])
-                        i = i + naluLen
-
-                elif self.avcPacketType == 0 :# 提取pps sps
-                    spsLen = (data[i+6] << 8) + data[i + 7]
-                    i += 8
-                    sps = data[i:i+spsLen]
-                    i += spsLen
-                    ppsLen = (data[i+1] << 8) + data[i+2]
-                    i += 3
-                    pps = data[i:i+ppsLen]
-                    naluSeq = naluHead + sps + naluHead + pps
-        elif self.tagType == 8:
-            atype = (data[0] & 0xf0) >> 4
-            if atype == 10:
-                adtsHeader = [0xff, 0xf1, 0x4c, 0x80,0x00,0x00,0xfc]
-                adtsLen = len(data)+7
-                adtsLen = adtsLen << 5
-                adtsLen = adtsLen | 0x1f
-                adtsHeader[4:6] = adtsLen.to_bytes(2,'big')
-                self.adts = adtsHeader + list(data)
+                readed = 5
+                if self.avcPacketType == 1: # [len + nalu,len + nalu]
+                    while (len(data)-5) > readed :
+                        naluLen = int.from_bytes(data[readed:readed+4], byteorder='big')
+                        readed += 4
+                        self.nalu += self.NALU_HEAD + data[readed:readed+naluLen]
+                        readed += naluLen
+                elif self.avcPacketType == 0: # sps pps
+                    spsLen = (data[readed+6] << 8) + data[readed + 7]
+                    readed += 8
+                    sps = data[readed:readed+spsLen]
+                    readed += spsLen
+                    ppsLen = (data[readed+1] << 8) + data[readed+2]
+                    readed += 3
+                    pps = data[readed:readed+ppsLen]
+                    # naluSeq = naluHead + sps + naluHead + pps
             else:
-                self.adts = list(data[1:self.dataSize])
-        self.previousTagSize = int.from_bytes(data[self.dataSize:], byteorder='big')
+                raise NameError("不支持的视频封装格式")
+        elif self.tagType == 8:
+            self.SoundFormat = (data[0] & 0xf0) >> 4
+            if self.SoundFormat == 10: # AAC type
+                self.aacPacketType = data[1]
+                self.adts = bytearray()
+                if self.aacPacketType == 1: # adts data
+                    data = data[2:self.dataSize]
+                    adtsHeader = [0xff, 0xf1, 0x4c, 0x80,0x00,0x00,0xfc]
+                    adtsLen = len(data)+7
+                    adtsLen = adtsLen << 5
+                    adtsLen = adtsLen | 0x1f
+                    adtsHeader[4:6] =  adtsLen.to_bytes(2,'big')
+                    self.adts = bytearray(adtsHeader) + data
+                elif self.aacPacketType == 0:
+                    pass
+            elif self.SoundFormat == 2: # MP3 type
+                self.audio = data[1:]
+            else:
+                raise NameError("未知的视频类型")
+        elif self.tagType == 18:
+            pass
+        else:
+            raise NameError("未知的tag type:" + str(self.tagType))
 
-    def getTagType(self):
-        '输出TagType名称'
-        return self.tagTypeItem[self.tagType]
+class FLV:
+
+    TAG_HEAD_LEN = 11
+
+    def setFile(self,filename):
+        self.tags = []
+        with open(filename, 'rb') as f:
+            self.hex = bytearray(f.read())
+        hexCount = len(self.hex)
+        readCount = 13
+
+        while hexCount > readCount:
+            tag = TAG()
+            tag.getHead(self.hex[readCount:readCount+self.TAG_HEAD_LEN])
+            # print("debug",tag.tagType,self.hex[readCount:readCount+self.TAG_HEAD_LEN])
+            readCount += self.TAG_HEAD_LEN
+            tag.setData(self.hex[readCount:readCount+tag.dataSize])
+            readCount += tag.dataSize + 4
+            self.tags.append(tag)
+
+H264DefaultHZ = 90
+
+if __name__ == "__main__":
+    import ts
+    flv = FLV()
+    flv.setFile("testcopy.flv")
     
+    tsFile = open("peng1.ts",'wb')
+    tsFile.write(ts.SDT())
+    tsFile.write(ts.PAT())
+    tsFile.write(ts.PMT())
 
-class FlvHeader:
-    name = "Flv Header"
-    def __init__(self,data) -> None:
-        self.data = data
-        self.previousTagSize = int.from_bytes(self.data[9:14], byteorder='big')
+    mp3File = open("peng.mp3",'wb')
 
-class Flv:
-    def __init__(self,flv) -> None:
-        self.header = FlvHeader(flv.read(13))
-        self.body = []
-        self.tagList = [] # del
-        while(True):
-            header = flv.read(11)
-            if(header == b''):
-                break
-            tag = FlvTag(header)
-            tag.setData(flv.read(tag.dataSize + 4))
-            self.body.append(tag)
-            # 展示标签列表
-            self.tagList.append(tag.getTagType()) # del
-
-def newFLv(pwd):
-    with open(pwd, 'rb') as flv:
-        return Flv(flv)
-
-
-ts = newFLv("./test.flv")
-
-
-h264DefaultHZ = 90
-
-import mpegts
-
-
-
-with open("testgen001.ts",'wb') as h:
-    h.write(mpegts.SDT())
-    h.write(mpegts.PAT())
-    h.write(mpegts.PMT())
-    for tag in ts.body:
+    count = 0
+    for tag in flv.tags:
         if tag.tagType == 9:
-            if tag.nalu == []:
+            count += 1
+            if tag.nalu == b'':
                 continue
-            dts = tag.timeStamp * h264DefaultHZ
-            pts = dts + (tag.compositionTime * h264DefaultHZ)
-            peshead = mpegts.PES(pts,dts,len(tag.nalu))
-            pes = peshead.data + bytes(tag.nalu)
-            byteData = mpegts.PACKET(pes,dts,True).getPack()
-            h.write(byteData)
-        if tag.tagType == 8:
-            
-            h.write(bytes(tag.adts))
+            dts = tag.timeStamp * H264DefaultHZ
+            pts = dts + (tag.compositionTime * H264DefaultHZ)
+            pes = ts.Pes.setES("Video",tag.nalu,pts,dts)
+            tsFile.write(ts.Packet.setPes(pes))
+        elif tag.tagType == 8:
+            dts = tag.timeStamp * H264DefaultHZ
+            if tag.SoundFormat == 2:
+                if tag.audio == b'':
+                    continue
+                mp3File.write(tag.audio)
+                pes = ts.Pes.setES("Audio",tag.audio,dts,dts)
+                tsFile.write(ts.Packet.setPes(pes))
+            elif tag.SoundFormat == 10:
+                if tag.adts == b'':
+                    continue
+                pes = ts.Pes.setES("Audio",tag.adts,dts,dts)
+                tsFile.write(ts.Packet.setPes(pes))
+
+    mp3File.close()
+    tsFile.close()
--- mpegts.py
+++ mpegts.py
@@ -1,38 +1,35 @@
 
+'''  处理ts解包器  '''
+
 class TsPacketHeader:
     def __init__(self,data) -> None:
         self.sync = data[0]
         self.error = data[1] >> 7  #是否报错
         self.payloadStart = data[1] >> 6 & 1 
         self.priority = data[1] >> 5 & 1 #优先级
-        data[1] &= 0x1f
-        self.pid = int.from_bytes(data[1:3],"big")
+        self.pid = int.from_bytes([data[1] & 0x1f,data[2]],"big")
         self.scrambling = data[3] >> 6 #传输加扰。
         self.adaption = data[3] >> 4 & 3
         self.continuity = data[3] & 0x0f
 
 class TsPacket:
     def __init__(self,data) -> None:
-        'adaption 01{1}仅含有效负载，10{2}仅含调整字段，11{3}含有调整字段和有效负载。为00解码器不进行处理'
-        self.header = TsPacket(data[0:4])
+        self.header = TsPacketHeader(data[0:4])
         self.body = []
+        self.adaptLen = data[4]
         if self.header.adaption >= 2:#存在调整字段
-            self.adaptLen = data[4]
             self.adapt = data[5:5+self.adaptLen]
             if self.header.adaption == 3:#同时存在es
                 self.body = data[5+self.adaptLen:]
             else:
                 self.body = []
-        elif self.header.adaptLen == 1:
+        elif self.header.adaption == 1:
             if self.header.payloadStart == 1:
                 startLen = 5 + data[5]
                 self.body = data[startLen:]
             else:
                 self.body = data[4:]
 
-# ts = Ts("test.ts")
-# for pkg in ts.body:
-#     pass
 class Ts:
     def __init__(self,pwd) -> None:
         self.body = []
@@ -43,153 +40,64 @@ class Ts:
                     break
                 self.body.append(TsPacket(tspck))
 
-def SDT():
-    bt = bytearray([0xff]*188)
-    hex = [
-        0x47, 0x40, 0x11, 0x10, 
-        0x00, 0x42, 0xF0, 0x25, 0x00, 0x01, 0xC1, 0x00, 0x00, 0xFF, 
-        0x01, 0xFF, 0x00, 0x01, 0xFC, 0x80, 0x14, 0x48, 0x12, 0x01, 
-        0x06, 0x46, 0x46, 0x6D, 0x70, 0x65, 0x67, 0x09, 0x53, 0x65, 
-        0x72, 0x76, 0x69, 0x63, 0x65, 0x30, 0x31, 0x77, 0x7C, 0x43, 
-        0xCA
-    ]
-    bt[0:45] = hex
-    return bt
 
-def PAT():
-    bt = bytearray([0xff]*188)
-    hex = [
-        0x47, 0x40, 0x00, 0x10, #ts packet hd
-        0x00, #adaption
-        0x00, 0xB0, 0x0D, 0x00, 0x01, 0xC1, 0x00, 0x00, 0x00, 0x01, 
-        0xF0, 0x00, 0x2A, 0xB1, 0x04, 0xB2
-    ]
-    bt[0:21] = hex
-    return bt
+#256
+VideoPid = 0x100 
+AudioPid = 0x101 
 
-def PMT():
-    bt = bytearray([0xff]*188)
-    hex = [
-        0x47, 0x50, 0x00, 0x10,
-        0x00,
-        0x02, 0xB0, 0x17, 0x00, 0x01, 0xC1, 0x00, 0x00, 0xE1, 0x00,
-        0xF0, 0x00, 0x1B, 0xE1, 0x00, 0xF0, 0x00, 0x0F, 0xE1, 0x01,
-        0xF0, 0x00, 0x2F, 0x44, 0xB9, 0x9B
-    ]
-    bt[0:31] = hex
-    return bt
-
-class PES():
-    _StartCode = [0x00, 0x00, 0x01]
-    _StreamId = {"video":0xe0,"audit":0xc0}
-    _PESLength = [0x00,0x00]
-    def __init__(self,pts,dts,dl) -> None:
-        self.data = bytearray([0]*19)
-        self.data[0:3] = [0x00, 0x00, 0x01]
-        self.data[3] = 0xe0
-        PesLen = 13 + dl
-        self.data[4:6] = [0,0]#[PesLen >> 8,PesLen & 0xff]
-        self.data[6] = 0x80 # pes-info信息
-        self.data[7] = 0xc0 # 默认存在dts和pts
-        self.data[8] = 0x0a # len
-
-        # pts
-        self.data[9] = (3 << 4) | ((pts>>30) << 1) | 1
-        gc = ((pts >> 15) & 0x7fff) << 1 | 1
-        self.data[10] = gc >> 8
-        self.data[11] = gc & 0xff
-        gc = ((pts & 0x7fff) << 1 ) | 1
-        self.data[12] = gc >> 8
-        self.data[13] = gc & 0xff
-
-        # dts
-        self.data[14] = (1 << 4) | ((dts>>30) << 1) | 1
-        gc = ((dts >> 15) & 0x7fff) << 1 | 1
-        self.data[15] = gc >> 8
-        self.data[16] = gc & 0xff
-        gc = ((dts & 0x7fff) << 1 ) | 1
-        self.data[17] = gc >> 8
-        self.data[18] = gc & 0xff
+# 提取PES到文件
 
 
-VIDEO_COUNT=0
-AUDIT_COUNT=0
+class Pes:
+    def __init__(self,data) -> None:
+        self.start_code_prefix = data[0:3]
+        self.stream_id = data[3]
+        self.pes_packet_length = int.from_bytes(data[4:6],'big')
+        self.pes_action = data[6:8]
+        self.pes_header_data_length = data[8]
+        self.pes_header_data = data[9:9+self.pes_header_data_length]
+        self.body = data[9+self.pes_header_data_length:]
+        # print(self.pes_header_data)
+        # print(self.body)
 
-class PACKET():
 
-    def __init__(self,r,dts,pcrflag) :
-        self.pack = bytearray()
-        countW = 0
-        while True:
-            countW += 1
-            if(len(r) == 0):
-                break
-            pack = bytearray([0xff]*188)
-            # 头部赋值。
-            index = 4 
-            pack[0:index] = self.tsHeader(pcrflag)
-            # adaptation 赋值只有首package才增加adaption
-            if pcrflag:
-                adapt = self.tsAdaptation(dts)
-                pack[index:index+len(adapt)] = adapt
-                index += len(adapt)
-                pcrflag = False
+# 单信道
+# 不做多路复用
+class AnalyzePes:
+    def __init__(self,filename) -> None:
+        self.PesVideo = []
+        self.PesAudio = []
 
-            need = 188 - index
+        TmpPesVideo = []
+        TmpPesAudio = []
+        
+        self.PesVideoCount = 1
+        self.PesAudioCount = 1
 
-            # 有ts packet 填充字段。 
-            if need > len(r): # 分段的尾部，或者开头的尾部。
-                if countW > 1:# 最后一段尾部
-                    # pack[1] |= 0x40
-                    pack[3] |= 0x30
-                pack[4] = 183 - len(r)
-                if pack[5] == 0xff:
-                    pack[5] = 0
-                index = 188 - len(r)
-                pack[index:188] = r
-            else:
-                pack[index:188] = r[:need]
+        ts = Ts(filename)
+        for pkg in ts.body:
+            if pkg.header.pid == VideoPid:
+                if pkg.header.payloadStart == 1:
+                    if self.PesVideoCount != 1:
+                        self.PesVideo.append(Pes(TmpPesVideo))
+                    self.PesVideoCount += 1
+                    TmpPesVideo = []
+                TmpPesVideo += pkg.body
+            if pkg.header.pid == AudioPid:
+                if pkg.header.payloadStart == 1:
+                    if self.PesAudioCount != 1:
+                        self.PesAudio.append(Pes(TmpPesAudio))
+                    self.PesAudioCount += 1
+                    TmpPesAudio = []
+                TmpPesAudio += pkg.body
+        self.PesVideo.append(Pes(TmpPesVideo))
+        self.PesVideo.append(Pes(TmpPesAudio))
 
-            r = r[need:]
-            self.pack += pack
-            if(len(pack) != 188):
-                print("debug-here",len(pack),index,pack[4],len(r))
-                exit()
-    
-    def getPack(self):
-        return self.pack
-    
-    def tsAdaptation(self,pcr):
-        h = bytearray(8)
-        # 8bit 表示是否时间戳发生变化
-        # 7bit 表示pes起始包
-        # 6bit 表示优先级，不管。
-        # 5bit 表示 是否有pcr
-        h[1] =  (1 << 6) | (1<<4)
-        # 4bit 表示 是否有ocpr
-        # 3bit 表示 是否有-
-        # 2bit 表示 是否有私域字段
-        # 1bit 表示 是否有拓展数据
-        h[2] = pcr >> 25
-        h[3] = (pcr >> 17) & 0xff
-        h[4] = (pcr >> 9) & 0xff
-        h[5] = (pcr >> 1) & 0xff
-        h[6] = ((pcr & 0x1) << 7) | 0x7e
-        h[7] = 0x00
-        h[0] = 7
-        return h
-    
-    def tsHeader(self,adapta=False):
-        global VIDEO_COUNT
-        'adapta 是否有拓展字段'
-        h = bytearray(4)
-        h[0] = 0x47
-        if adapta:
-            h[1] = 1 << 6
-            h[3] = 3 << 4
-        else:
-            h[3] = 1 << 4
-        h[1] |= 1
-        h[3] |= VIDEO_COUNT
-        VIDEO_COUNT = (VIDEO_COUNT + 1 ) % 16
-        return h
+pes = AnalyzePes("test.ts")
+with open('test_mpegts.h264','wb') as ts:
+    for pkg in pes.PesVideo:
+        ts.write(bytes(pkg.body))
+
+with open('test_mpegts.aac','wb') as ts:
+    for pkg in pes.PesAudio:
+        ts.write(bytes(pkg.body))
